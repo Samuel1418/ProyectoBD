3811acc6417208fe53e8621e08811d13424a85d1
Samuel1418 <sgomezpintos@danielcastelao.org>
2019-05-31 13:14:40 +0200

    Comentados metodos conectar y crear tablas ademas de eliminar los void

--- src/Exemplo_BD/ConectarBaseEjemplo.java
+++ src/Exemplo_BD/ConectarBaseEjemplo.java
@@ -15,6 +15,12 @@ import java.sql.Statement;
  * @author Samuel
  */
 public class ConectarBaseEjemplo {
+    
+    /*
+    Metodo que nos conecta al archivo de hola.db o en su defecto nos crea el archivo 
+    con ese nombre. Hay que dar la ruta al metodo getConnection y nos devolverá true
+    si conecta a la tabla y false si no consigue establecer la conexión
+    */
     public static void connect() {
         Connection conn = null;
         try {
@@ -39,12 +45,16 @@ public class ConectarBaseEjemplo {
     }
  
     
-    
-    public static void createNewTable() {
-        // SQLite connection string
+    /*
+    Método para crear la tabla Clase.
+    Con el String sql realizamos el create tal cual sería en la propia base de datos
+    Añadimos el codigo del String al método execute despues de conectar a la base
+    la clase Statement con el método createStatement
+    Si hubiese algún error saltaría la excepcion 
+    */
+    public static String createNewTable() {
         String url = "jdbc:sqlite:D:\\Clase\\SQLMan\\hola.db";
         
-        // SQL statement for creating a new table
         String sql = "CREATE TABLE IF NOT EXISTS Clase (\n"
                 + "	Nombre text ,\n"
                 + "	NumeroID integer PRIMARY KEY,\n"
@@ -56,10 +66,18 @@ public class ConectarBaseEjemplo {
             stmt.execute(sql);
         } catch (SQLException e) {
             System.out.println(e.getMessage());
-        }
+        }return url;
     }
     
-    public static void createNewTable2() {
+    
+    /*
+    Método para crear la tabla Pais.
+    Con el String sql realizamos el create tal cual sería en la propia base de datos
+    Añadimos el codigo del String al método execute despues de conectar a la base
+    la clase Statement con el método createStatement.
+    Si hubiese algún error saltaría la excepcion 
+    */
+    public static String createNewTable2() {
         // SQLite connection string
         String url = "jdbc:sqlite:D:\\Clase\\SQLMan\\hola.db";
         
@@ -74,7 +92,7 @@ public class ConectarBaseEjemplo {
             stmt.execute(sql1);
         } catch (SQLException e) {
             System.out.println(e.getMessage());
-        }
+        }return url;
     }
     
 }
